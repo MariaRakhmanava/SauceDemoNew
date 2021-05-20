a18fde9eb30476e589711273c1bb25b49cf04f24
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-05-20 17:43:28 +0300

    Added pages and tests classes, README.md, gitignore file

--- src/main/java/pages/CheckoutOverviewPage.java
+++ src/main/java/pages/CheckoutOverviewPage.java
@@ -15,7 +15,8 @@ public class CheckoutOverviewPage extends HeaderPage {
     private static final String PRODUCT_ITEM_NAME = "//*[contains(text(), '%s')]";
     private static final String PRODUCT_QUANTITY = PRODUCT_ITEM_NAME + "/ancestor::*[@class='cart_item']//*[@class='cart_quantity']";
     private static final String PRODUCT_PRICE = PRODUCT_ITEM_NAME + "/ancestor::*[@class='cart_item']//*[@class='inventory_item_price']";
-    private static final By PAYMENT_INFORMATION_FIELD = By.xpath("//*[contains(text(), 'Payment Information:')]/parent::*[@class='summary_info']/*[contains(text(), 'SauceCard')]");
+    private static final By PAYMENT_INFORMATION_FIELD = By.xpath("//*[contains(text(), 'Payment Information')]/parent::*[@class='summary_info']/*[contains(text(), 'SauceCard')]");
+    private static final By SHIPPING_INFORMATION_FIELD = By.xpath("//*[contains(text(),'Shipping Information')]/following-sibling::*[1]");
     private static final By ITEM_TOTAL_FIELD = By.xpath("//*[@class='summary_subtotal_label']");
     private static final By TAX_FIELD = By.xpath("//*[@class='summary_tax_label']");
     private static final By TOTAL_FIELD = By.xpath("//*[@class='summary_total_label']");
