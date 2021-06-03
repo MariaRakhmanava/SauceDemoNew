3299d8e94c3d32f7a9e9cfb1a463c6725b203550
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-06-03 22:10:44 +0300

    Added pages and tests java.classes, the list of checks README.md, and gitignore file

--- src/main/java/pages/ProductsPage.java
+++ src/main/java/pages/ProductsPage.java
@@ -1,9 +1,11 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
@@ -125,5 +127,9 @@ public class ProductsPage extends GeneralPartPage {
     public CartPage goToCartByCartIcon() {
         super.goToCart();
         return new CartPage(driver);
+    }
+
+    public void moveToProductDetailsPage(String element) {
+        driver.findElement(this.getProductLink(element)).click();
     }
 }
--- src/test/java/tests/ProductDetailsTest.java
+++ src/test/java/tests/ProductDetailsTest.java
@@ -7,8 +7,8 @@ public class ProductDetailsTest extends BaseTest implements iTestConstants {
     @Test
     public void checkProductCharacteristicsTest() {
         loginPage.openPage()
-                 .login(STANDARD_USER_LOGIN, VALID_PASSWORD);
-        driver.findElement(productsPage.getProductLink(SAUCE_LABS_BACKPACK_PRODUCT)).click();
+                 .login(STANDARD_USER_LOGIN, VALID_PASSWORD)
+                 .moveToProductDetailsPage(SAUCE_LABS_BACKPACK_PRODUCT);
         Assert.assertEquals(productDetailsPage.getProductItemName(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT);
         Assert.assertEquals(productDetailsPage.getProductDescription(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT_DESCRIPTION);
         Assert.assertEquals(productDetailsPage.getProductPrice(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT_PRICE);
