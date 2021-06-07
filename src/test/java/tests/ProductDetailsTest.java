9b95f70a1bbf6b54d23aa0ba773940dd3c834326
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-06-07 12:31:25 +0300

    Added new files

--- src/test/java/tests/ProductDetailsTest.java
+++ src/test/java/tests/ProductDetailsTest.java
@@ -4,13 +4,27 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProductDetailsTest extends BaseTest implements ITestConstants {
-    @Test
-    public void checkProductCharacteristicsTest() {
+    @Test(retryAnalyzer = Retry.class)
+    public void checkProductCharacteristicNameTest() {
         loginPage.openPage()
                  .login(standardUser());
         productsPage.clickProductNameToGoToProductDetailsPage(SAUCE_LABS_BACKPACK_PRODUCT);
         Assert.assertEquals(productDetailsPage.getProductItemName(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT);
-        Assert.assertEquals(productDetailsPage.getProductDescription(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT_DESCRIPTION);
+    }
+
+    @Test(retryAnalyzer = Retry.class)
+    public void checkProductCharacteristicPriceTest() {
+        loginPage.openPage()
+                .login(standardUser());
+        productsPage.clickProductNameToGoToProductDetailsPage(SAUCE_LABS_BACKPACK_PRODUCT);
         Assert.assertEquals(productDetailsPage.getProductPrice(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT_PRICE);
+    }
+
+    @Test(retryAnalyzer = Retry.class)
+    public void checkProductCharacteristicDescriptionTest() {
+        loginPage.openPage()
+                .login(standardUser());
+        productsPage.clickProductNameToGoToProductDetailsPage(SAUCE_LABS_BACKPACK_PRODUCT);
+        Assert.assertEquals(productDetailsPage.getProductDescription(SAUCE_LABS_BACKPACK_PRODUCT), SAUCE_LABS_BACKPACK_PRODUCT_DESCRIPTION);
     }
 }
