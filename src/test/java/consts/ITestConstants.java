2533d55b3d14f39731d9b47900baf2a4a296df4a
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-06-07 13:02:05 +0300

    Added new files

---
+++ src/test/java/consts/ITestConstants.java
@@ -0,0 +1,33 @@
+package consts;
+
+public interface ITestConstants {
+    String VALID_LOGIN = "standard_user";
+    String VALID_PASSWORD = "secret_sauce";
+
+    String BY_PRICE_HIGH_TO_LOW_PRODUCTS_SORTING_PRINCIPLE = "Price (high to low)";
+    String BY_PRICE_LOW_TO_HIGH_PRODUCTS_SORTING_PRINCIPLE = "Price (low to high)";
+    String BY_NAME_A_TO_Z_PRODUCTS_SORTING_PRINCIPLE = "Name (A to Z)";
+    String BY_NAME_Z_TO_A_PRODUCTS_SORTING_PRINCIPLE = "Name (Z to A)";
+
+    String SAUCE_LABS_BOLT_T_SHIRT_PRODUCT = "Sauce Labs Bolt T-Shirt";
+    String SAUCE_LABS_ONESIE_PRODUCT = "Sauce Labs Onesie";
+    String SAUCE_LABS_BACKPACK_PRODUCT = "Sauce Labs Backpack";
+    String SAUCE_LABS_BIKE_LIGHT_PRODUCT = "Sauce Labs Bike Light";
+    String SAUCE_LABS_FLEECE_JACKET_PRODUCT = "Sauce Labs Fleece Jacket";
+    String TEST_ALL_THE_THINGS_T_SHIRT_RED_PRODUCT = "Test.allTheThings() T-Shirt (Red)";
+
+    String FIRST_NAME_INPUT_VALUE = "Jane";
+    String LAST_NAME_INPUT_VALUE = "Doe";
+    String POSTAL_CODE_INPUT_VALUE = "220018";
+
+    String USERNAME_REQUIRED_ERROR_MESSAGE_TEXT = "Epic sadface: Username is required";
+    String PASSWORD_REQUIRED_ERROR_MESSAGE_TEXT = "Epic sadface: Password is required";
+    String NO_MATCHES_ERROR_MESSAGE_TEXT = "Epic sadface: Username and password do not match any user in this service";
+    String FIRST_NAME_REQUIRED_ERROR_MESSAGE_TEXT = "Error: First Name is required";
+    String LAST_NAME_REQUIRED_ERROR_MESSAGE_TEXT = "Error: Last Name is required";
+    String POSTAL_CODE_REQUIRED_ERROR_MESSAGE_TEXT = "Error: Postal Code is required";
+
+    String SAUCE_LABS_BACKPACK_PRODUCT_PRICE ="$29.99";
+
+    String SAUCE_LABS_BACKPACK_PRODUCT_DESCRIPTION ="carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.";
+}
